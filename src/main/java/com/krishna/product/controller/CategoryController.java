3eb7b779df89b84a411bb22d67e173d72a718030
Krishna Madhav <getgoingzazz@gmail.com>
2026-03-03 03:26:00 +0530

    Created a basic version of controller for creating category

--- src/main/java/com/krishna/product/controller/CategoryController.java
+++ src/main/java/com/krishna/product/controller/CategoryController.java
@@ -1,5 +1,11 @@
 package com.krishna.product.controller;
 
+import com.krishna.product.dto.CategoryDTO;
+import com.krishna.product.entity.Category;
+import com.krishna.product.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/categories")
 public class CategoryController {
 
+    @Autowired
+    private CategoryService categoryService;
+
     // create a category
+    @PostMapping
+    public Category createCategory(@RequestBody Category category){
+
+        categoryService.createCategory(category);
+        return category;
+    }
+
     // get a category based on id
     // get all categories
     // delete a category
--- src/main/java/com/krishna/product/service/CategoryService.java
+++ src/main/java/com/krishna/product/service/CategoryService.java
@@ -1,8 +1,20 @@
 package com.krishna.product.service;
 
+import com.krishna.product.dto.CategoryDTO;
+import com.krishna.product.entity.Category;
+import com.krishna.product.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class CategoryService {
 
+    private CategoryRepository categoryRepository;
+
+    public Category createCategory(Category category){
+
+        Category newCategory = categoryRepository.save(category);
+        return newCategory;
+    }
 }
